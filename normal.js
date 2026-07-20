7e8efe8acae6e8054f5062c100dd448625cc8626
ayusshh66 <adonisjefrey66@gmail.com>
2026-07-20 23:19:16 +0530

    day 1 of learning typescript

---
+++ normal.js
@@ -0,0 +1,10 @@
+const User = new String("ayush")
+// console.log(typeof (prac));
+
+// console.log(User.__proto__);
+
+// const gameName = new String('hitesh-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+console.log(typeof User);
